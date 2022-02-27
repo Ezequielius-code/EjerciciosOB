e10a5edd4e8788921e6b468e0ca5644ec3ee930b
unknown <bruttoezequiel@gmail.com>
2022-02-27 18:01:55 -0300

    EjercicioTemas1y2

---
+++ Introducción a la programación/EjerciciosTemas1Y2/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int numero1 = 10;
+        System.out.println(numero1);
+
+        numero1 = 15;
+        System.out.println(numero1);
+
+        int numeros[] = { 98, 126, 84, 986, 764 };
+        System.out.println(numeros[0]);
+    }
+}

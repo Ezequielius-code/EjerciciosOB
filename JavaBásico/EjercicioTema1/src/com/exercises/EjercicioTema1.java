2b8493615e3b61a0522b6307e6989514b17d1726
unknown <bruttoezequiel@gmail.com>
2022-03-05 14:23:14 -0300

    EjercicioTema1Completo

---
+++ JavaBásico/EjercicioTema1/src/com/exercises/EjercicioTema1.java
@@ -0,0 +1,20 @@
+package com.exercises;
+
+public class EjercicioTema1 {
+
+    public static void main(String[] args) {
+        byte num0 = 8;
+        short num1 = 16;
+        int num2 = 32;
+        long num3 = 64;
+        float num4 = 32.00f;
+        double num5 = 64.00;
+        boolean allDataTypes = false;
+        char char1 = 'z';
+        String string1 = "Task done";
+
+        System.out.printf("ALL DATA TYPES:\nByte: " + num0 + "\nShort: " + num1 + "\nInteger: " + num2 +
+                          "\nLong: " + num3 + "\nFloating point: " + num4 + "\nDouble: " + num5 +
+                          "\nBoolean: " + allDataTypes + "\nCharacter: " + char1 + "\nString: " + string1);
+    }
+}
